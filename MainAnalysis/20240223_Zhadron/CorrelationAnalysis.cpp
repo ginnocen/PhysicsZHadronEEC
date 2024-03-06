58da23bc692dbc27e978060f2b4b9556a238740b
Yen-Jie Lee <yenjie@mit.edu>
2024-03-06 11:50:18 -0500

    Clean up and clarification

--- MainAnalysis/20240223_Zhadron/CorrelationAnalysis.cpp
+++ MainAnalysis/20240223_Zhadron/CorrelationAnalysis.cpp
@@ -7,62 +7,55 @@
 #include <TCut.h>
 #include <TLegend.h>
 #include <TNtuple.h>
-
 #include <iostream>
 
 using namespace std;
-
 #include "utilities.h"     // Yen-Jie's random utility functions
 #include "Messenger.h"     // Yi's Messengers for reading data files
 #include "CommandLine.h"   // Yi's Commandline bundle
 #include "ProgressBar.h"   // Yi's fish progress bar
-
-
 //============================================================//
 // Define analysis parameters
 //============================================================//
-
 class Parameters {
-  public:
-  Parameters(double MinZPT, double MaxZPT, double MinTrackPT, double MaxTrackPT,
-             int MinHiBin = 0, int MaxHiBin = 200, bool mix = false, bool isGen = false,
-             double scaleFactor = 1.0, double nMix = 1)
-    : MinZPT(MinZPT), MaxZPT(MaxZPT), MinTrackPT(MinTrackPT), MaxTrackPT(MaxTrackPT),
-      MinHiBin(MinHiBin), MaxHiBin(MaxHiBin), mix(mix), isGen(isGen), scaleFactor(scaleFactor), nMix(nMix) {}
+public:
+    Parameters( float MinZPT, float MaxZPT, float MinTrackPT, float MaxTrackPT, int MinHiBin = 0, int MaxHiBin = 200, bool mix = false, float scaleFactor = 1.0, float nMix = 1)
+    : MinZPT(MinZPT), MaxZPT(MaxZPT), MinTrackPT(MinTrackPT), MaxTrackPT(MaxTrackPT), MinHiBin(MinHiBin), MaxHiBin(MaxHiBin), mix(mix), scaleFactor(scaleFactor), nMix(nMix) {}
 
-  double MinZPT;        // Lower limit of zpt
-  double MaxZPT;        // Upper limit of zpt
-  double MinTrackPT;    // Lower limit of pt
-  double MaxTrackPT;    // Upper limit of pt
-  int MinHiBin;         // Lower limit of hiBin
-  int MaxHiBin;         // Upper limit of hiBin
-  bool isGen;           // isGen flag
-  double scaleFactor;   // Scale factor
-  string input;         // Input file name
-  string output;        // Output file name
-  string mixFile;       // Mix File name
-  int nThread;          // Number of Threads
-  int nChunk;           // Process the Nth chunk
-  bool mix;             // Mix flag
-  int nMix;             // Number of mixed events
+    string input;          // Input file name
+    string output;         // Output file name
+    string mixFile;        // Mix File name
+    float MinZPT;          // Lower limit of Z pt
+    float MaxZPT;          // Upper limit of Z pt
+    float MinTrackPT;      // Lower limit of track pt
+    float MaxTrackPT;      // Upper limit of track pt
+    bool isSelfMixing;     // isSelfMixing flag
+    float scaleFactor;     // Scale factor
+    int MinHiBin;          // Lower limit of hiBin
+    int MaxHiBin;          // Upper limit of hiBin
+    int nThread;           // Number of Threads
+    int nChunk;            // Process the Nth chunk
+    bool mix;              // Mix flag
+    int nMix;              // Number of mixed events
 
-  void printParameters() const {
-    cout << "MinZPT: " << MinZPT << endl;
-    cout << "zPtCfutH: " << MaxZPT << endl;
-    cout << "MinTrackPT: " << MinTrackPT << endl;
-    cout << "MaxTrackPT: " << MaxTrackPT << endl;
-    cout << "MinHiBin: " << MinHiBin << endl;
-    cout << "MaxHiBin: " << MaxHiBin << endl;
-    cout << "mix: " << mix << endl;
-    cout << "isGen: " << isGen << endl;
-    cout << "input: " << input << endl;
-    cout << "output: " << output << endl;
-    cout << "nChunk/nThread: " << nChunk << "/" << nThread << endl;
-    if (mix) cout <<"Event mixing!"<<endl;
-    cout << "scaleFactor: " << scaleFactor << endl;
-  }
+    void printParameters() const {
+        cout << "Input file: " << input << endl;
+        cout << "Output: " << output << endl;
+        cout << "MixFile: " << mixFile << endl;
+        cout << "MinZPT: " << MinZPT << endl;
+        cout << "MaxZPT: " << MaxZPT << endl;
+        cout << "MinTrackPT: " << MinTrackPT << endl;
+        cout << "MaxTrackPT: " << MaxTrackPT << endl;
+        cout << "MinHiBin: " << MinHiBin << endl;
+        cout << "MaxHiBin: " << MaxHiBin << endl;
+        cout << "mix: " << mix << endl;
+        cout << "isSelfMixing: " << isSelfMixing << endl;
+        cout << "nChunk/nThread: " << nChunk << "/" << nThread << endl;
+
+        if (mix) cout << "Event mixing!" << endl;
+        cout << "scaleFactor: " << scaleFactor << endl;
+    }
 };
-
 
 //======= eventSelection =====================================//
 // Check if the event mass eventSelection criteria
@@ -96,75 +89,75 @@ bool trackSelection(ZHadronMessenger *b, Parameters par, int j) {
     return true;
 }
 
-
 //============================================================//
 // Z hadron dphi calculation
 //============================================================//
-double getDphi(ZHadronMessenger *b, ZHadronMessenger *MMix, TH2D *h, const Parameters& par) {
 
-    double nZ=0;
+float getDphi(ZHadronMessenger *MZSignal, ZHadronMessenger *MMix, TH2D *h, const Parameters& par) {
+
+    float nZ = 0;
     h->Sumw2();
     par.printParameters();
-    unsigned long nEntry = b->GetEntries()*par.scaleFactor;
-    unsigned long iStart = nEntry*(par.nChunk-1)/par.nThread;
-    unsigned long iEnd = nEntry*par.nChunk/par.nThread;
-    ProgressBar Bar(cout, iEnd-iStart);
+    unsigned long nEntry = MZSignal->GetEntries() * par.scaleFactor;
+    unsigned long iStart = nEntry * (par.nChunk - 1) / par.nThread;
+    unsigned long iEnd = nEntry * par.nChunk / par.nThread;
+    unsigned int targetMix = ((par.nMix - 1) * par.mix + 1);
+
+    ProgressBar Bar(cout, iEnd - iStart);
     Bar.SetStyle(1);
 
-    for (unsigned long i=iStart;i<iEnd;i++) {
-       bool foundZ=false;
-       b->GetEntry(i);
-       if (i%30000==0){
-          Bar.Update(i-iStart);
+    for (unsigned long i = iStart; i < iEnd; i++) {
+       bool foundZ = false;
+       MZSignal->GetEntry(i);
+       if (i % 100000 == 0) {
+          Bar.Update(i - iStart);
           Bar.Print();
        }
-       
-       // check if the event pass the selection criteria
-       if (eventSelection(b, par)) {
-          double zPhi = (*b->zPhi)[0];
-          double zEta = (*b->zEta)[0];	  
 
-	  double mix_i = i;
-	  
-	  // find a mixed event
-	  for (unsigned int nMix=0;nMix<((par.nMix-1)*par.mix+1);nMix++) {
-	     bool foundMix = 0;
-	     if (par.mix) {
-	        while (foundMix==0) {
-		
-	           mix_i = (mix_i+1);
-		   if (mix_i>=MMix->GetEntries()) mix_i=0;
-		   if (i==mix_i)  break;
-		   MMix->GetEntry(mix_i);
-		   if (eventSelection(MMix, par)) foundMix=1;
-	        }
-	     }
-	  
-	     if (foundMix==0&&par.mix) {
-	        cout <<"Can not find a mixed event!!! Event = "<<i<<endl;
-	        break;
-	     }
-	     
-	     nZ+=(b->NCollWeight);    // Ncoll reweighting in the event level.
-	     for (unsigned long j=0;j<(par.mix ? MMix->trackPhi->size(): b->trackPhi->size());j++) {
-                if (!trackSelection((par.mix ? MMix: b), par, j)) continue;  
-                double trackDphi  = par.mix ? DeltaPhi((*MMix->trackPhi)[j], zPhi) : DeltaPhi((*b->trackPhi)[j], zPhi);
-		double trackDphi2 = par.mix ? DeltaPhi(zPhi,(*MMix->trackPhi)[j]) : DeltaPhi(zPhi, (*b->trackPhi)[j]);
-	        double trackDeta  = par.mix ? fabs((*MMix->trackEta)[j]- zEta) : fabs((*b->trackEta)[j]- zEta);
-		double weight = par.mix ? (MMix->NCollWeight)*(*MMix->trackWeight)[j]*(MMix->ZWeight) : (b->NCollWeight)*(*b->trackWeight)[j]*(b->ZWeight); //(*b->trackResidualWeight)[j]*
-		
-		h->Fill(trackDeta,trackDphi,weight);
- 	        h->Fill(-trackDeta,trackDphi,weight);
- 	        h->Fill(trackDeta,trackDphi2,weight);
- 	        h->Fill(-trackDeta,trackDphi2,weight);
-	     }
+       // Check if the event passes the selection criteria
+       if (eventSelection(MZSignal, par)) {
+          float zPhi = (*MZSignal->zPhi)[0];
+          float zEta = (*MZSignal->zEta)[0];    
+
+          float mix_i = i;
+
+          // Find a mixed event
+          for (unsigned int nMix = 0; nMix < targetMix; nMix++) {
+             bool foundMix = false;
+             if (par.mix) {
+                while (!foundMix) {
+                   mix_i = (mix_i + 1);
+                   if (mix_i >= MMix->GetEntries()) mix_i = 0;
+                   if (i == mix_i) break;
+                   MMix->GetEntry(mix_i);
+                   if (eventSelection(MMix, par)) foundMix = true;
+                }
+             }
+
+             if (!foundMix && par.mix) {
+                cout << "Cannot find a mixed event!!! Event = " << i << endl;
+                break;
+             }
+
+             nZ += (MZSignal->NCollWeight);  // Ncoll reweighting at the event level.
+             for (unsigned long j = 0; j < (par.mix ? MMix->trackPhi->size() : MZSignal->trackPhi->size()); j++) {
+                if (!trackSelection((par.mix ? MMix : MZSignal), par, j)) continue;
+                float trackDphi  = par.mix ? DeltaPhi((*MMix->trackPhi)[j], zPhi) : DeltaPhi((*MZSignal->trackPhi)[j], zPhi);
+                float trackDphi2 = par.mix ? DeltaPhi(zPhi, (*MMix->trackPhi)[j]) : DeltaPhi(zPhi, (*MZSignal->trackPhi)[j]);
+                float trackDeta  = par.mix ? fabs((*MMix->trackEta)[j] - zEta) : fabs((*MZSignal->trackEta)[j] - zEta);
+                float weight = par.mix ? (MMix->NCollWeight) * (*MMix->trackWeight)[j] * (MMix->ZWeight) : (MZSignal->NCollWeight) * (*MZSignal->trackWeight)[j] * (MZSignal->ZWeight);
+
+                h->Fill(trackDeta, trackDphi, weight);
+                h->Fill(-trackDeta, trackDphi, weight);
+                h->Fill(trackDeta, trackDphi2, weight);
+                h->Fill(-trackDeta, trackDphi2, weight);
+             }
           }
        }
     }
-    cout <<"done"<<nZ<<endl;
+    cout << "done" << nZ << endl;
     return nZ;
 }
-
 
 class DataAnalyzer {
 public:
@@ -221,33 +214,11 @@ public:
   }
 };
 
-
-
 //============================================================//
 // Main analysis
 //============================================================//
-
-int compareDataMCLoop(Parameters &par)
-{
-   TCanvas *c = new TCanvas("c", "", 800, 800);
-
-   // Analyze Data
-   DataAnalyzer analyzerData(par.input.c_str(), par.mixFile.c_str(), "Data");
-   analyzerData.analyze(par);
-
-
-   TFile *outf = new TFile(par.output.c_str(), "recreate");
-   analyzerData.writeHistograms(outf);
-
-   cout << "done!" << endl;
-   cout << outf->GetName() << endl;
-   return 1;
-}
-
-
 int main(int argc, char *argv[])
 {
-
    // Read command line
    CommandLine CL(argc, argv);
 
@@ -256,13 +227,14 @@ int main(int argc, char *argv[])
    string MixFile    = CL.Get("MixFile", "sample/HISingleMuon.root");               // Input Mix file
    string Output     = CL.Get("Output",  "output.root");                             // Output file
 
+   bool IsSelfMixing = CL.GetBool("IsSelfMixing", true);  // Determine if the analysis is self-mixing      
    bool IsData       = CL.GetBool("IsData", false);       // Determines whether the analysis is being run on actual data.
    bool IsPP         = CL.GetBool("IsPP", false);         // Flag to indicate if the analysis is for Proton-Proton collisions.
-   double Fraction   = CL.GetDouble("Fraction", 1.00);    // Fraction of event processed in the sample
-   double MinZPT     = CL.GetDouble("MinZPT", 40);        // Minimum Z particle transverse momentum threshold for event selection.
-   double MinTrackPT = CL.GetDouble("MinTrackPT", 1);     // Minimum track transverse momentum threshold for track selection.
-   double MaxZPT     = CL.GetDouble("MaxZPT", 120);       // Maximum Z particle transverse momentum threshold for event selection.
-   double MaxTrackPT = CL.GetDouble("MaxTrackPT", 2);     // Maximum track transverse momentum threshold for track selection.
+   float Fraction   = CL.GetDouble("Fraction", 1.00);    // Fraction of event processed in the sample
+   float MinZPT     = CL.GetDouble("MinZPT", 40);        // Minimum Z particle transverse momentum threshold for event selection.
+   float MinTrackPT = CL.GetDouble("MinTrackPT", 1);     // Minimum track transverse momentum threshold for track selection.
+   float MaxZPT     = CL.GetDouble("MaxZPT", 120);       // Maximum Z particle transverse momentum threshold for event selection.
+   float MaxTrackPT = CL.GetDouble("MaxTrackPT", 2);     // Maximum track transverse momentum threshold for track selection.
    int MaxHiBin      = CL.GetInt("MaxHiBin", 0);          // Maximum hiBin value for event selection. hiBin is a variable often used in heavy-ion collision experiments.
    int MinHiBin      = CL.GetInt("MinHiBin", 200);        // Minimum hiBin value for event selection.
    int nThread       = CL.GetInt("nThread", 1);           // The number of threads to be used for parallel processing.
@@ -271,7 +243,7 @@ int main(int argc, char *argv[])
 
    // Parameter sets
    Parameters par(MinZPT, MaxZPT, MinTrackPT, MaxTrackPT, MaxHiBin, MinHiBin);
-   par.isGen = 0;
+   par.isSelfMixing = IsSelfMixing;
    par.mix = 0;
    par.scaleFactor = Fraction;
    par.input = Input;
@@ -280,7 +252,20 @@ int main(int argc, char *argv[])
    par.nThread = nThread;
    par.nChunk = nChunk;
    par.nMix = nMix;
-
-   compareDataMCLoop(par);
    
+   if (IsSelfMixing && Input!=MixFile) {
+      cout <<"Error! Self-mixing mode but assigned different input and mix files. Please check the macro."<<endl;
+      return -1;
+   }
+
+   TCanvas *c = new TCanvas("c", "", 800, 800);
+
+   // Analyze Data
+   DataAnalyzer analyzer(par.input.c_str(), par.mixFile.c_str(), "Data");
+   analyzer.analyze(par);
+
+   TFile *outf = new TFile(par.output.c_str(), "recreate");
+   analyzer.writeHistograms(outf);
+
+   cout << "done!" << outf->GetName() << endl;
 }
