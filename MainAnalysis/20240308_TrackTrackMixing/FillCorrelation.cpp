4bdf6d6a70e07ba4c1232efd498db197d58d9cd6
Luna <yi.chen@cern.ch>
2024-04-03 09:03:18 -0500

    update track track mixing

--- MainAnalysis/20240308_TrackTrackMixing/FillCorrelation.cpp
+++ MainAnalysis/20240308_TrackTrackMixing/FillCorrelation.cpp
@@ -21,7 +21,9 @@ int main(int argc, char *argv[])
    string BackgroundFileName = SelfMixingMode ? "NONE" : CL.Get("Background");
    string OutputFileName     = CL.Get("Output");
    double Fraction           = CL.GetDouble("Fraction", 1.00);
+   double MinPT              = CL.GetDouble("MinPT", 3.00);
    bool IsPP                 = CL.GetBool("IsPP", false);
+   bool IsReco               = CL.GetBool("IsReco", true);
    bool CheckZ               = CL.GetBool("CheckZ", true);
    bool CheckSignalHiBin     = CL.GetBool("CheckSignalHiBin", true);
    double HiBinMin           = CL.GetDouble("HiBinMin", 0);
@@ -31,12 +33,28 @@ int main(int argc, char *argv[])
    double SignalHFMax        = CL.GetDouble("SignalHFMax", 155015.70);
    double HFShift            = CL.GetDouble("HFShift", 767.2);
    int OverSample            = SelfMixingMode ? 1 : CL.GetInt("OverSample", 100);
+   int SubEvent              = CL.GetInteger("SubEvent", -1);
 
    TFile OutputFile(OutputFileName.c_str(), "RECREATE");
 
    TH1D HCount("HCount", ";", 10, 0, 10);
+
+   // Z distributions
+   TH1D HZY("HZY", ";y_{Z}", 100, -3, 3);
+   TH1D HZPT("HZPT", ";p_{T,Z}", 100, 0, 100);
+   TH1D HZMass("HZMass", ";m_{Z}", 100, 40, 140);
+   
+   // Z-hadron correlations
+   TH1D HZHEta("HZHEta", ";y_{Z} - #eta_{h}", 100, -3, 3);
+   TH1D HZHPhi("HZHPhi", ";#Delta#phi_{Z,h}", 100, -3, 3);
+   TH1D HZHPT("HZHPT", ";p_{T,H}", 100, 0, 100);
+   
+   // Track-track correlations
    TH2D HEtaPhi("HEtaPhi", ";#Delta#eta;#Delta#phi", 100, -3, 3, 100, -M_PI, M_PI);
+   TH1D HEta("HEta", ";#Delta#eta", 100, -3, 3);
    TH1D HPhi("HPhi", ";#Delta#phi", 100, -M_PI, M_PI);
+   TH1D HPT1("HPT1", ";p_{T}", 100, 0, 50);
+   TH1D HPT2("HPT2", ";p_{T}", 100, 0, 50);
 
    TFile SignalFile(SignalFileName.c_str());
    TFile BackgroundFile(BackgroundFileName.c_str());
@@ -57,8 +75,11 @@ int main(int argc, char *argv[])
    {
       MZHadron.GetEntry(iE);
 
-      Bar.Update(iE);
-      Bar.Print();
+      if(EntryCount < 300 || (iE % (EntryCount / 250) == 0))
+      {
+         Bar.Update(iE);
+         Bar.Print();
+      }
 
       if(IsPP == false)
       {
@@ -73,21 +94,47 @@ int main(int argc, char *argv[])
             if(MZHadron.SignalHF >= SignalHFMax)  continue;
          }
       }
+         
+      double EventWeight1 = MZHadron.VZWeight * MZHadron.ZWeight;
 
-      if(IsPP == true && MZHadron.NVertex != 1)
+      if(IsPP == true && IsReco == true && MZHadron.NVertex != 1)
          continue;
       if(CheckZ == true)
       {
-         if(MZHadron.zY == nullptr)
-            continue;
-         if(MZHadron.zY->size() == 0)
-            continue;
-         if(MZHadron.zPt->at(0) < 20)
-            continue;
-         if(MZHadron.zMass->at(0) > 120 || MZHadron.zMass->at(0) < 60)
-            continue;
-         if(MZHadron.zY->at(0) < -2.4 || MZHadron.zY->at(0) > 2.4)
-            continue;
+         if(IsReco == true)
+         {
+            if(MZHadron.zY == nullptr)
+               continue;
+            if(MZHadron.zY->size() == 0)
+               continue;
+            if(MZHadron.zPt->at(0) < 20)
+               continue;
+            if(MZHadron.zMass->at(0) > 120 || MZHadron.zMass->at(0) < 60)
+               continue;
+            if(MZHadron.zY->at(0) < -2.4 || MZHadron.zY->at(0) > 2.4)
+               continue;
+
+            HZY.Fill(MZHadron.zY->at(0), EventWeight1);
+            HZPT.Fill(MZHadron.zPt->at(0), EventWeight1);
+            HZMass.Fill(MZHadron.zMass->at(0), EventWeight1);
+         }
+         else
+         {
+            if(MZHadron.genZY == nullptr)
+               continue;
+            if(MZHadron.genZY->size() == 0)
+               continue;
+            if(MZHadron.genZPt->at(0) < 20)
+               continue;
+            if(MZHadron.genZMass->at(0) > 120 || MZHadron.genZMass->at(0) < 60)
+               continue;
+            if(MZHadron.genZY->at(0) < -2.4 || MZHadron.genZY->at(0) > 2.4)
+               continue;
+
+            HZY.Fill(MZHadron.genZY->at(0), EventWeight1);
+            HZPT.Fill(MZHadron.genZPt->at(0), EventWeight1);
+            HZMass.Fill(MZHadron.genZMass->at(0), EventWeight1);
+         }
       }
 
       for(int iO = 0; iO < OverSample; iO++)
@@ -104,40 +151,98 @@ int main(int argc, char *argv[])
                BackgroundHF = MZHadronBackground.SignalHF;
             } while(BackgroundHF < SignalHFMin - HFShift || BackgroundHF > SignalHFMax - HFShift);
          }
+      
+         double EventWeight = EventWeight1;
+         if(SelfMixingMode == false)
+            EventWeight = EventWeight1 * MZHadronBackground.VZWeight * MZHadronBackground.ZWeight;
 
          int N1 = (MZHadron.trackPt != nullptr) ? MZHadron.trackPt->size() : 0;
          int N2 = N1;
          if(SelfMixingMode == false)
             N2 = (MZHadronBackground.trackPt != nullptr) ? MZHadronBackground.trackPt->size() : 0;
 
-         double EventWeight = MZHadron.VZWeight * MZHadron.ZWeight;
-
          double SumWeight1 = 0;
+      
+         if(CheckZ == true)
+         {
+            for(int iP = 0; iP < N2; iP++)
+            {
+               bool IsMu = SelfMixingMode ? MZHadron.trackMuTagged->at(iP) : MZHadronBackground.trackMuTagged->at(iP);
+               double PT = SelfMixingMode ? MZHadron.trackPt->at(iP) : MZHadronBackground.trackPt->at(iP);
+               double Eta = SelfMixingMode ? MZHadron.trackEta->at(iP) : MZHadronBackground.trackEta->at(iP);
+               double Phi = SelfMixingMode ? MZHadron.trackPhi->at(iP) : MZHadronBackground.trackPhi->at(iP);
+               double TrackWeight = SelfMixingMode ? MZHadron.trackWeight->at(iP) : MZHadronBackground.trackWeight->at(iP);
+               TrackWeight = TrackWeight * EventWeight;
+               
+               double DEta = (IsReco ? MZHadron.zY->at(0) : MZHadron.genZY->at(0)) - Eta;
+               double DPhi = GetDeltaPhi((IsReco ? MZHadron.zPhi->at(0) : MZHadron.genZPhi->at(0)), Phi);
+
+               if(IsMu == true)
+                  continue;
+               if(PT < MinPT)
+                  continue;
+            
+               int SubEvent2 = SelfMixingMode ? MZHadron.subevent->at(iP) : MZHadronBackground.subevent->at(iP);
+               if(SubEvent >= 0 && SubEvent2 != SubEvent)
+                  continue;
+
+               HZHEta.Fill(+DEta, 0.5 * TrackWeight);
+               HZHEta.Fill(-DEta, 0.5 * TrackWeight);
+               HZHPhi.Fill(+DPhi, 0.5 * TrackWeight);
+               HZHPhi.Fill(-DPhi, 0.5 * TrackWeight);
+               HZHPT.Fill(PT, TrackWeight);
+            }
+         }
 
          for(int iP1 = 0; iP1 < N1; iP1++)
          {
+            double PT1 = MZHadron.trackPt->at(iP1);
             double Eta1 = MZHadron.trackEta->at(iP1);
             double Phi1 = MZHadron.trackPhi->at(iP1);
             double Weight1 = MZHadron.trackWeight->at(iP1);
+            
+            if(MZHadron.trackMuTagged->at(iP1) == true)
+               continue;
+            if(PT1 < MinPT)
+               continue;
+
+            if(SubEvent >= 0 && MZHadron.subevent->at(iP1) != SubEvent)
+               continue;
+
             SumWeight1 = SumWeight1 + Weight1;
 
             for(int iP2 = 0; iP2 < N2; iP2++)
             {
                if(SelfMixingMode == true && iP1 == iP2)
                   continue;
-
+            
+               bool IsMu = SelfMixingMode ? MZHadron.trackMuTagged->at(iP2) : MZHadronBackground.trackMuTagged->at(iP2);
+               double PT2 = SelfMixingMode ? MZHadron.trackPt->at(iP2) : MZHadronBackground.trackPt->at(iP2);
                double Eta2 = SelfMixingMode ? MZHadron.trackEta->at(iP2) : MZHadronBackground.trackEta->at(iP2);
                double Phi2 = SelfMixingMode ? MZHadron.trackPhi->at(iP2) : MZHadronBackground.trackPhi->at(iP2);
                double Weight2 = SelfMixingMode ? MZHadron.trackWeight->at(iP2) : MZHadronBackground.trackWeight->at(iP2);
-               
+               int SubEvent2 = SelfMixingMode ? MZHadron.subevent->at(iP2) : MZHadronBackground.subevent->at(iP2);
+
+               if(IsMu == true)
+                  continue;
+               if(PT2 < MinPT)
+                  continue;
+
+               if(SubEvent >= 0 && SubEvent2 != SubEvent)
+                  continue;
+
                double Weight = Weight1 * Weight2 * EventWeight;
 
                HEtaPhi.Fill(Eta1 - Eta2, GetDeltaPhi(Phi1, Phi2), 0.25 * Weight);
                HEtaPhi.Fill(Eta2 - Eta1, GetDeltaPhi(Phi1, Phi2), 0.25 * Weight);
                HEtaPhi.Fill(Eta1 - Eta2, -GetDeltaPhi(Phi1, Phi2), 0.25 * Weight);
                HEtaPhi.Fill(Eta2 - Eta1, -GetDeltaPhi(Phi1, Phi2), 0.25 * Weight);
+               HEta.Fill(Eta1 - Eta2, 0.5 * Weight);
+               HEta.Fill(Eta2 - Eta1, 0.5 * Weight);
                HPhi.Fill(+GetDeltaPhi(Phi1, Phi2), 0.5 * Weight);
                HPhi.Fill(-GetDeltaPhi(Phi1, Phi2), 0.5 * Weight);
+               HPT1.Fill(max(PT1, PT2), Weight);
+               HPT2.Fill(min(PT1, PT2), Weight);
             }
          }
          
@@ -159,8 +264,17 @@ int main(int argc, char *argv[])
    OutputFile.cd();
 
    HCount.Write();
+   HZY.Write();
+   HZPT.Write();
+   HZMass.Write();
+   HZHEta.Write();
+   HZHPhi.Write();
+   HZHPT.Write();
    HEtaPhi.Write();
+   HEta.Write();
    HPhi.Write();
+   HPT1.Write();
+   HPT2.Write();
 
    OutputFile.Close();
 
--- MainAnalysis/20240308_TrackTrackMixing/QuickPlot.cpp
+++ MainAnalysis/20240308_TrackTrackMixing/QuickPlot.cpp
@@ -19,70 +19,112 @@ int main(int argc, char *argv[])
    CommandLine CL(argc, argv);
 
    vector<string> InputFileNames = CL.GetStringVector("Input");
+   vector<double> Coefficients = CL.GetDoubleVector("Coefficients");
+   vector<string> Histograms = CL.GetStringVector("Histograms");
+   string OutputFileName = CL.Get("Output");
+
    int N = InputFileNames.size();
+
+   Coefficients.insert(Coefficients.end(), 1, N);
 
    vector<TFile *> InputFiles(N);
    for(int i = 0; i < N; i++)
       InputFiles[i] = new TFile(InputFileNames[i].c_str());
 
-   vector<TH1D *> H1(N);
    vector<TH2D *> H2(N);
-   for(int i = 0; i < N; i++)
-   {
-      H1[i] = (TH1D *)InputFiles[i]->Get("HPhi");
-      H2[i] = (TH2D *)InputFiles[i]->Get("HEtaPhi");
 
-      TH1D *HCount = (TH1D *)InputFiles[i]->Get("HCount");
-      
-      H1[i]->Scale(1 / HCount->GetBinContent(1));
-      H2[i]->Scale(1 / HCount->GetBinContent(1));
-   }
-
-   PdfFileHelper PdfFile("Plots.pdf");
+   PdfFileHelper PdfFile(OutputFileName);
    PdfFile.AddTextPage("Juicy plots");
 
-   for(int i = 0; i < N; i++)
+   for(string H : Histograms)
    {
-      PdfFile.AddPlot(H1[i]);
-      PdfFile.AddTextPage(Form("Sum of all entries = %.2f", H1[i]->Integral()));
-      cout << InputFileNames[i] << " " << H1[i]->Integral() << endl;
-      PdfFile.AddPlot(H2[i], "colz");
+      vector<TH1D *> H1(N);
+      for(int i = 0; i < N; i++)
+      {
+         H1[i] = (TH1D *)InputFiles[i]->Get(H.c_str());
+         TH1D *HCount = (TH1D *)InputFiles[i]->Get("HCount");
+         H1[i]->Scale(1 / HCount->GetBinContent(1));
+
+         // H2[i] = (TH2D *)InputFiles[i]->Get("HEtaPhi");
+         // H2[i]->Scale(1 / HCount->GetBinContent(1));
+      }
+
+      for(int i = 0; i < N; i++)
+      {
+         PdfFile.AddPlot(H1[i]);
+         PdfFile.AddTextPage(Form("Sum of all entries = %.2f", H1[i]->Integral()));
+         cout << InputFileNames[i] << " " << H1[i]->Integral() << endl;
+         // PdfFile.AddPlot(H2[i], "colz");
+      }
+
+      double WorldMax = 0;
+      double WorldMin = 9999999;
+      for(int i = 0; i < N; i++)
+      {
+         WorldMax = max(WorldMax, H1[i]->GetMaximum());
+         WorldMin = min(WorldMin, H1[i]->GetMinimum());
+      }
+      
+      TCanvas Canvas;
+
+      double XMin = H1[0]->GetXaxis()->GetBinLowEdge(1);
+      double XMax = H1[0]->GetXaxis()->GetBinUpEdge(H1[0]->GetNbinsX());
+
+      TH2D HWorld("HWorld", ";;", 100, XMin, XMax, 100, WorldMax * -0.5, WorldMax * 2.5);
+      HWorld.GetXaxis()->SetTitle(H1[0]->GetXaxis()->GetTitle());
+      HWorld.SetStats(0);
+      HWorld.Draw();
+
+      TLegend Legend(0.2, 0.85, 0.5, 0.60);
+      Legend.SetBorderSize(0);
+      Legend.SetFillStyle(0);
+      Legend.SetTextFont(42);
+      Legend.SetTextSize(0.035);
+
+      for(int i = 0; i < N; i++)
+      {
+         H1[i]->SetLineWidth(2);
+         H1[i]->SetLineColor(Colors[i]);
+         H1[i]->Draw("hist same");
+         Legend.AddEntry(H1[i], Form("%s (%.3f) [%.0f]", InputFileNames[i].c_str(), H1[i]->Integral(), Coefficients[i]), "l");
+      }
+
+      TH1D *HDiff = (TH1D *)H1[0]->Clone("HDiff");
+      HDiff->SetLineWidth(2);
+      HDiff->SetLineColor(Colors[N]);
+      HDiff->SetLineStyle(kDashed);
+      HDiff->Scale(Coefficients[0]);
+      for(int i = 1; i < N; i++)
+         HDiff->Add(H1[i], Coefficients[i]);
+      Legend.AddEntry(HDiff, Form("Combined (%.3f)", HDiff->Integral()), "l");
+      HDiff->Draw("hist same");
+
+      Legend.Draw();
+
+      PdfFile.AddCanvas(Canvas);
+
+      Canvas.SetLogy();
+
+      if(WorldMin <= 0)
+         WorldMin = WorldMax * 0.00001;
+
+      TH2D HWorld2("HWorld2", ";;", 100, XMin, XMax, 100, WorldMin / 5, WorldMax * 50);
+      HWorld2.GetXaxis()->SetTitle(H1[0]->GetXaxis()->GetTitle());
+      HWorld2.SetStats(0);
+      HWorld2.Draw();
+      for(int i = 0; i < N; i++)
+         H1[i]->Draw("hist same");
+      HDiff->Draw("hist same");
+      Legend.Draw();
+
+      PdfFile.AddCanvas(Canvas);
+
+      HDiff->SetLineStyle(kSolid);
+      HDiff->SetMarkerStyle(20);
+      HDiff->SetMarkerColor(HDiff->GetLineColor());
+      // PdfFile.AddPlot(HDiff, "hist");
+      PdfFile.AddPlot(HDiff, "");
    }
-
-   TCanvas Canvas;
-
-   TH2D HWorld("HWorld", ";#Delta#phi;", 100, -M_PI, M_PI, 100, 0, 12000);
-   HWorld.SetStats(0);
-   HWorld.Draw();
-
-   TLegend Legend(0.2, 0.85, 0.5, 0.60);
-   Legend.SetBorderSize(0);
-   Legend.SetFillStyle(0);
-   Legend.SetTextFont(42);
-   Legend.SetTextSize(0.035);
-
-   for(int i = 0; i < N; i++)
-   {
-      H1[i]->SetLineWidth(2);
-      H1[i]->SetLineColor(Colors[i]);
-      H1[i]->Draw("hist same");
-      Legend.AddEntry(H1[i], Form("%s (%.0f)", InputFileNames[i].c_str(), H1[i]->Integral()), "l");
-   }
-
-   Legend.Draw();
-
-   PdfFile.AddCanvas(Canvas);
-
-   Canvas.SetLogy();
-   
-   TH2D HWorld2("HWorld2", ";#Delta#phi;", 100, -M_PI, M_PI, 100, 1, 500000);
-   HWorld2.SetStats(0);
-   HWorld2.Draw();
-   for(int i = 0; i < N; i++)
-      H1[i]->Draw("hist same");
-   Legend.Draw();
-   
-   PdfFile.AddCanvas(Canvas);
 
    PdfFile.AddTimeStampPage();
    PdfFile.Close();
