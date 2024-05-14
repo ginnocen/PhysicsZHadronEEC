c4272b6f345057de7be4e35281357a5eeaa47db6
Yen-Jie Lee <yenjie@mit.edu>
2024-05-14 11:55:17 -0400

    clean up

--- MainAnalysis/20240223_Zhadron/CorrelationAnalysis.cpp
+++ MainAnalysis/20240223_Zhadron/CorrelationAnalysis.cpp
@@ -31,6 +31,7 @@ public:
     float MinTrackPT;      // Lower limit of track pt
     float MaxTrackPT;      // Upper limit of track pt
     float scaleFactor;     // Scale factor
+    float shift;           // shift in sumHF when doing mb matching
     int MinHiBin;          // Lower limit of hiBin
     int MaxHiBin;          // Upper limit of hiBin
     int nThread;           // Number of Threads
@@ -44,6 +45,7 @@ public:
     bool isPUReject;       // Flag to reject PU sample for systemaitcs.
     bool isHiBinUp;        // Flag to do systematics with HiBinUp
     bool isHiBinDown;      // Flag to do systematics with HiBinDown
+    bool isPP;             // Flag to check if this is a PP analysis
 
    void printParameters() const {
        cout << "Input file: " << input << endl;
@@ -56,6 +58,7 @@ public:
        cout << "isSelfMixing: " << (isSelfMixing ? "true" : "false") << endl;
        cout << "isGenZ: " << (isGenZ ? "true" : "false") << endl;
        cout << "Scale factor: " << scaleFactor << endl;
+       cout << "SumHF shift: " << shift << endl;
        cout << "MinHiBin: " << MinHiBin << endl;
        cout << "MaxHiBin: " << MaxHiBin << endl;
        cout << "Number of Threads: " << nThread << endl;
@@ -106,7 +109,6 @@ bool eventSelection(ZHadronMessenger *b, const Parameters& par) {
    if (fabs((par.isGenZ ? (*b->genZEta)[0] : (*b->zEta)[0]))>2) return 0;
    if ((par.isGenZ ? (*b->genZPt)[0] : (*b->zPt)[0])<par.MinZPT) return 0;
    if ((par.isGenZ ? (*b->genZPt)[0] : (*b->zPt)[0])>par.MaxZPT) return 0;
-
    foundZ=1;   
    return foundZ;
 }
@@ -123,14 +125,14 @@ bool trackSelection(ZHadronMessenger *b, Parameters par, int j) {
 }
 
 // ======= Define mixed event matching criteria
-bool matching(ZHadronMessenger *a, ZHadronMessenger *b, double shift=1000) {
+bool matching(ZHadronMessenger *a, ZHadronMessenger *b, double shift) {
 
 // 1036 is the maxima of SignalHF in pythia
 //   double shift = 1018.0541;
 //   double shift = //1268.69;
 //    if (a->hiHF<97.13&&b->hiHF<97.13) return 1;
     if (a->SignalHF<shift*1.05&&b->SignalHF<shift*1.05) return 1;
-    if ((b->SignalHF/(a->SignalHF-shift))<1.02&&b->SignalHF/(a->SignalHF-shift)>1) return 1;
+    if ((b->SignalHF/(a->SignalHF-shift))<1.02&&b->SignalHF/(a->SignalHF-shift)>0.98) return 1;
     return 0;
 }
 
@@ -161,7 +163,7 @@ float getDphi(ZHadronMessenger *MZSignal, ZHadronMessenger *MMix, ZHadronMesseng
        // Check if the event passes the selection criteria
        if (eventSelection(MZSignal, par)) {
           // Find a mixed akeevent
-	  float zEta = (par.isGenZ ? (*MZSignal->genZEta)[0] : (*MZSignal->zEta)[0]);
+	  float zY = (par.isGenZ ? (*MZSignal->genZY)[0] : (*MZSignal->zY)[0]);
 	  float zPhi = (par.isGenZ ? (*MZSignal->genZPhi)[0] : (*MZSignal->zPhi)[0]);
 	  
           for (unsigned int nMix = 0; nMix < targetMix; nMix++) {
@@ -176,7 +178,7 @@ float getDphi(ZHadronMessenger *MZSignal, ZHadronMessenger *MMix, ZHadronMesseng
                    if (par.isSelfMixing) {
 		      if (eventSelection(MMixEvt, par)&&par.isSelfMixing&&i!=mix_i) foundMix = true;
                    } else {
-		      if (matching(MZSignal,MMixEvt)&&!par.isSelfMixing) foundMix = true;
+		      if (matching(MZSignal,MMixEvt,par.shift)&&!par.isSelfMixing) foundMix = true;
 
 		   }
 		}
@@ -191,10 +193,10 @@ float getDphi(ZHadronMessenger *MZSignal, ZHadronMessenger *MMix, ZHadronMesseng
                 if (!trackSelection((par.mix ? MMix : MZSignal), par, j)) continue;
                 float trackDphi  = par.mix ? DeltaPhi((*MMix->trackPhi)[j], zPhi) : DeltaPhi((*MZSignal->trackPhi)[j], zPhi);
                 float trackDphi2 = par.mix ? DeltaPhi(zPhi, (*MMix->trackPhi)[j]) : DeltaPhi(zPhi, (*MZSignal->trackPhi)[j]);
-                float trackDeta  = par.mix ? fabs((*MMix->trackEta)[j] - zEta) : fabs((*MZSignal->trackEta)[j] - zEta);
+                float trackDeta  = par.mix ? fabs((*MMix->trackEta)[j] - zY) : fabs((*MZSignal->trackEta)[j] - zY);
                 //float weight = par.mix ? (MMix->NCollWeight) * (*MMix->trackWeight)[j] * (MMix->ZWeight) : (MZSignal->NCollWeight) * (*MZSignal->trackWeight)[j] * (MZSignal->ZWeight);
                 float weight = (par.mix&&par.isSelfMixing) ? (MMix->ZWeight) : (MZSignal->ZWeight);
-		weight*=(par.mix ? (*MMix->trackWeight)[j] : (*MZSignal->trackWeight)[j]);
+		weight*=(par.mix ? (*MMix->trackWeight)[j]*(*MMix->trackResidualWeight)[j] : (*MZSignal->trackWeight)[j]*(*MZSignal->trackResidualWeight)[j]);
                 h->Fill( trackDeta, trackDphi , weight);
                 h->Fill(-trackDeta, trackDphi , weight);
                 h->Fill( trackDeta, trackDphi2, weight);
@@ -218,8 +220,7 @@ float getDphi(ZHadronMessenger *MZSignal, ZHadronMessenger *MMix, ZHadronMesseng
 class DataAnalyzer {
 public:
   DataAnalyzer(const char* filename, const char* mixFilename, const char* outFilename, const char *mytitle = "Data") :
-     inf(new TFile(filename)), MZHadron(new ZHadronMessenger(*inf,string("Tree"))), mixFile(new TFile(mixFilename)), mixFileClone(new TFile(mixFilename)), MMix(new ZHadronMessenger(*mixFile,string("Tree"))), MMixEvt(new ZHadronMessenger(*mixFileClone,string("Tree"),true)), title(mytitle), outf(new TFile(outFilename, "recreate"))
-  {
+     inf(new TFile(filename)), MZHadron(new ZHadronMessenger(*inf,string("Tree"))), mixFile(new TFile(mixFilename)), mixFileClone(new TFile(mixFilename)), MMix(new ZHadronMessenger(*mixFile,string("Tree"))), MMixEvt(new ZHadronMessenger(*mixFileClone,string("Tree"),true)), title(mytitle), outf(new TFile(outFilename, "recreate"))  {
      outf->cd();
      ntDiagnose = new TNtuple ("ntDiagnose","","zPt:nTrk:hiBin:SignalHF:nTrkMix:hiBinMix:SignalHFMix:nMix");     
   }
@@ -274,35 +275,41 @@ int main(int argc, char *argv[])
 {
    if (printHelpMessage(argc, argv)) return 0;
 
-   // Read command line
    CommandLine CL(argc, argv);
    float MinZPT      = CL.GetDouble("MinZPT", 40);         // Minimum Z particle transverse momentum threshold for event selection.
    float MaxZPT      = CL.GetDouble("MaxZPT", 200);        // Maximum Z particle transverse momentum threshold for event selection.
    float MinTrackPT  = CL.GetDouble("MinTrackPT", 1);      // Minimum track transverse momentum threshold for track selection.
    float MaxTrackPT  = CL.GetDouble("MaxTrackPT", 2);      // Maximum track transverse momentum threshold for track selection.
-   int MinHiBin      = CL.GetInt   ("MinHiBin", 0);        // Minimum hiBin value for event selection.
-   int MaxHiBin      = CL.GetInt   ("MaxHiBin", 200);      // Maximum hiBin value for event selection. hiBin.
+   int   MinHiBin    = CL.GetInt   ("MinHiBin", 0);        // Minimum hiBin value for event selection.
+   int   MaxHiBin    = CL.GetInt   ("MaxHiBin", 200);      // Maximum hiBin value for event selection.
+   bool  IsData      = CL.GetBool  ("IsData", false);      // Determines whether the analysis is being run on actual data.
+   bool  IsPP        = CL.GetBool  ("IsPP", false);        // Flag to indicate if the analysis is for Proton-Proton collisions.
+   cout <<MinTrackPT<<" "<<MaxTrackPT<<endl;
+   if (IsPP) {
+      MinHiBin=-2;
+      MaxHiBin=0;
+   }
 
    Parameters par(MinZPT, MaxZPT, MinTrackPT, MaxTrackPT, MinHiBin, MaxHiBin);
-   par.input         = CL.Get      ("Input",   "mergedSample/HISingleMuon-v4.root");                // Input file
-   par.mixFile       = CL.Get      ("MixFile", "mergedSample/HISingleMuon-v4.root");                // Input Mix file
-   par.output        = CL.Get      ("Output",  "output.root");                             // Output file
+   par.input         = CL.Get      ("Input",   "mergedSample/HISingleMuon-v5.root");            // Input file
+   par.mixFile       = CL.Get      ("MixFile", "mergedSample/HISingleMuon-v5.root");            // Input Mix file
+   par.output        = CL.Get      ("Output",  "output.root");                             	// Output file
    par.isSelfMixing  = CL.GetBool  ("IsSelfMixing", true); // Determine if the analysis is self-mixing
    par.isGenZ        = CL.GetBool  ("IsGenZ", false);      // Determine if the analysis is using Gen level Z     
-   bool IsData       = CL.GetBool  ("IsData", false);      // Determines whether the analysis is being run on actual data.
-   bool IsPP         = CL.GetBool  ("IsPP", false);        // Flag to indicate if the analysis is for Proton-Proton collisions.
    par.isPUReject    = CL.GetBool  ("IsPUReject", false);  // Flag to reject PU sample for systemaitcs.
    par.isMuTagged    = CL.GetBool  ("IsMuTagged", true);   // Default is true
-   par.isHiBinUp     = CL.GetBool  ("IsHiBinUp", false);    // Default is false
-   par.isHiBinDown   = CL.GetBool  ("IsHiBinUp", false);    // Default is false
+   par.isHiBinUp     = CL.GetBool  ("IsHiBinUp", false);   // Default is false
+   par.isHiBinDown   = CL.GetBool  ("IsHiBinDown", false); // Default is false
    par.scaleFactor   = CL.GetDouble("Fraction", 1.00);     // Fraction of event processed in the sample
    par.nThread       = CL.GetInt   ("nThread", 1);         // The number of threads to be used for parallel processing.
    par.nChunk        = CL.GetInt   ("nChunk", 1);          // Specifies which chunk (segment) of the data to process, used in parallel processing.
    par.nMix          = CL.GetInt   ("nMix", 10);           // Number of mixed events to be considered in the analysis.
+   par.shift         = CL.GetDouble("Shift", 971.74);       // Shift of sumHF in MB matching
    par.mix = 0;
+   par.isPP = IsPP;
    
    if (checkError(par)) return -1;
-       
+          
    // Analyze Data
    DataAnalyzer analyzer(par.input.c_str(), par.mixFile.c_str(), par.output.c_str(), "Data");
    analyzer.analyze(par);
