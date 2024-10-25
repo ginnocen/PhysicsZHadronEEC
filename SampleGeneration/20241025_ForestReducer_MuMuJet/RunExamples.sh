566307edb8a9879bbab5ab988b8eb728ee39b43e
Gian Michele Innocenti <ginnocen@cern.ch>
2024-10-26 01:15:51 +0200

    add MC sample

--- SampleGeneration/20241025_ForestReducer_MuMuJet/RunExamples.sh
+++ SampleGeneration/20241025_ForestReducer_MuMuJet/RunExamples.sh
@@ -6,7 +6,7 @@ mkdir -p Output/
 ### Data ###
 ############
 
-./Execute --Input Samples/HiForestMiniAOD_2.root \
+./Execute --Input Samples/HiForestMiniAOD_2_Data.root \
    --Output Output/output.root \
    --Year 2018 \
    --TrackEfficiencyPath ${ProjectBase}/CommonCode/root/ \
@@ -15,4 +15,20 @@ mkdir -p Output/
    --IsPP false \
    --IsBackground false \
    --CheckZ true
+
+
+############
+### MC Gen ###
+############
+
+./Execute --Input Samples/HiForestMiniAOD_30_MC.root \
+   --Output Output/output.root \
+   --Year 2018 \
+   --TrackEfficiencyPath ${ProjectBase}/CommonCode/root/ \
+   --DoGenLevel true \
+   --IsData false \
+   --IsPP false \
+   --IsBackground false \
+   --CheckZ true
+
 
