e98ff80d5500422280c67e8fcbe7d3ffc3f376d0
Gian Michele Innocenti <ginnocen@cern.ch>
2024-10-26 22:10:26 -0400

    update cleaning

--- SampleGeneration/20241025_ForestReducer_MuMuJet/clean.sh
+++ SampleGeneration/20241025_ForestReducer_MuMuJet/clean.sh
@@ -6,6 +6,9 @@ rm -rf Output
 rm SkimReco.root
 rm list.txt
 rm -rf output
+rm *.txt*
+rm SkimReco.root
+rm .DS_Store
 
 cd /home/ginnocen/CMSSW_13_2_6_patch2/src
 cmsenv
@@ -19,5 +22,5 @@ make
 cd ..
 cd SampleGeneration/20241025_ForestReducer_MuMuJet
 make
-
+rm SkimReco.root
 
